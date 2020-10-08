f105945a2dc461d7013e4f711157b60ab2a830a7
pipe473 <bedoyafelipe743@gmail.com>
2020-10-08 12:52:45 +0200

    Segunda app con ionic consumiendo plugins de ionic, geolocalizacion, con promesas y callbacks

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Geolocation, Geoposition } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public latitudPromesa;
+  public longitudPromesa;
+  public latitudObservable;
+  public longitudObservable;
 
-  constructor() {}
+  constructor(private geolocation: Geolocation) 
+  {
+    this.latitudPromesa = 0;
+    this.longitudPromesa = 0;
+    this.latitudObservable = 0;
+    this.longitudObservable = 0;
+  }
+
+  obtenerLocalizacionPromesa():void{
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.latitudPromesa = resp.coords.latitude
+      this.longitudPromesa = resp.coords.longitude
+    }).catch((err) =>{
+      console.log('Error getting location', err);      
+    });
+  }
+
+  obtenerLocalizacionObservable():void
+  {
+    let watch = this.geolocation.watchPosition();
+    watch.subscribe((data:Geoposition) => 
+    {
+      this.latitudObservable = data.coords.latitude
+      this.longitudObservable = data.coords.longitude
+      console.log(data);
+      
+    })
+  }
 
 }
